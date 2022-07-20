ae65c2d210df3da41c90a801af9c5f803e2553d8
DESKTOP-KVCQHCD\SSAFY <yerim8373@naver.com>
2022-07-20 16:25:25 +0900

    feat(feature-back / stomp): stomp setting

---
+++ backend-java/src/main/java/com/ssafy/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.ssafy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@EnableWebSocketMessageBroker
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp/chat") // WebSocket 또는 SockJS Client가 웹소켓 핸드셰이크 커넥션을 생성할 경로
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub"); //Client에서 SEND 요청 처리
+        registry.enableSimpleBroker("/sub");
+    }
+}
