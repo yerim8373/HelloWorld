bef0dc68aa4efb0d4172d770862f6a48aee32b92
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-07-22 15:46:59 +0900

    feat([folders] common,pages,public / [files] headernav,logoimage,profileimage): create component
    create component HeaderNav,ProfileImage, MenuBtn / add asset images

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import { wrapper } from "../store";
+import HeaderNav from "../components/common/HeaderNav";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <HeaderNav />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
