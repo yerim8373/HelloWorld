28cd962de1fe6b1398efee797221196e473d7db6
홍주성 <hjs8482@naver.com>
2022-07-22 21:00:29 +0900

    refactor(dto): add annotation build,getter,setter,constructors, etc

--- backend-java/src/main/java/com/ssafy/api/dto/SignInDTO.java
+++ backend-java/src/main/java/com/ssafy/api/dto/SignInDTO.java
@@ -1,8 +1,12 @@
 package com.ssafy.api.dto;
 
-import lombok.Getter;
+import lombok.*;
 
+@Builder
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignInDTO {
     private String email;
     private String pw;
