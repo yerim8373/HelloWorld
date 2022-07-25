4356cde7a573977721ad931cc9fe3dd7c1686182
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-07-26 01:26:43 +0900

    feat([page] login): create login page
    button-component : add secondary color / sheet-component : update padding

--- frontend/components/common/HeaderNavAuth.js
+++ frontend/components/common/HeaderNavAuth.js
@@ -1,15 +1,30 @@
 import LogoImage from "./LogoImage";
 import classes from "./HeaderNavAuth.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const HeaderNavAuth = () => {
+  const router = useRouter();
+  console.log(router.pathname.includes("find"));
   return (
     <div className={classes.HeaderNav}>
       <nav className={classes.HeaderNav_nav}>
         <div className={classes.HeaderNav_link_btns}>
-          <LogoImage color="logoBlack" width="50" height="50"></LogoImage>
+          <LogoImage
+            color={router.pathname.includes("auth") ? "logoBlack" : "logoWhite"}
+            width="50"
+            height="50"
+          ></LogoImage>
           <Link href="/">
-            <h3 className={classes.HeaderNav_brand}>HelloWorld</h3>
+            <h3
+              className={
+                router.pathname.includes("find")
+                  ? classes.HeaderNav_brand_black
+                  : classes.HeaderNav_brand_white
+              }
+            >
+              HelloWorld
+            </h3>
           </Link>
         </div>
       </nav>
