50596c0e3fa4ec54dff48a81704cc534c2debe9c
김성찬 <kimsc0714@naver.com>
2022-07-25 16:51:09 +0900

    feat(modal): add modal root DOM

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,13 +2,17 @@ import "../styles/minireset.min.css";
 import "../styles/pretendard.css";
 import "../styles/globals.css";
 import { wrapper } from "../store";
-import HeaderNav from "../components/common/HeaderNav";
+import HeaderNav from "../components/common/HeaderNav";;
 
 function MyApp({ Component, pageProps }) {
+
+
   return (
     <>
       <HeaderNav />
       <Component {...pageProps} />
+
+      <div id="modal-root"></div>
     </>
   );
 }
