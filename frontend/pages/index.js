2b7c152588cf8bbb729853fea8d7ab94c8b0f384
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-07-25 23:08:23 +0900

    feat(sheet.js): create sheet component

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,13 @@
-import LodingSpinner from "../components/common/LoadingSpinner";
+import LoadingSpinner from "../components/common/LoadingSpinner";
 import Input from "../components/common/Input";
 import Sheet from "../components/common/sheet";
-
 export default function LandingPage() {
   return (
     <>
       <h1>랜딩 페이지</h1>
       {/* 로딩 스피너 시험용 */}
       {/* <LodingSpinner viewCount={5} /> */}
-      {/* input 시험용 */}S
+      {/* input 시험용 */}
       {/* <form>
         <Input
           id={"Email"}
@@ -17,7 +16,8 @@ export default function LandingPage() {
         />
         <Input id={"비밀번호"} type={"password"} />
       </form> */}
-      <Sheet />
+      {/* <Sheet>
+      </Sheet> */}
     </>
   );
 }
