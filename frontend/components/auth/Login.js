ada679a5d52099afb79f5d9c0587ce19633b1f20
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-07-26 01:36:55 +0900

    docs(readme.md): add done list ("bell") 220725

--- frontend/components/auth/Login.js
+++ frontend/components/auth/Login.js
@@ -8,6 +8,13 @@ import classes from "../auth/Login.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
+// 유효성 검사 설정
+// useRef를 통한 현재 input 값 읽기
+// useEffect를 통한 useRef 변경마다, 유효성 체크
+// 디바운싱을 통한 최적화
+// 조건에 따라, 부적합한 값인 경우, 그에 따른 알맞은 에러값 모두 송출
+// 마음의 숙제 : 할 수 있다면 비즈니스 로직으로 빼서 처리하자!!
+
 const Login = () => {
   const router = useRouter();
   function routerPushHandler() {
