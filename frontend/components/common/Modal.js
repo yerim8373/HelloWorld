f5bb03ad1b1b8ffa2c5a17189558d12e848b27bf
김성찬 <kimsc0714@naver.com>
2022-07-25 17:25:00 +0900

    docs(modal): add API comments

--- frontend/components/common/Modal.js
+++ frontend/components/common/Modal.js
@@ -1,4 +1,17 @@
-import { useState } from 'react'
+/**
+ * 모달 창 & 후면 오버레이 컴포넌트
+ *
+ * opened {boolean}: 모달 창 표시 여부
+ * handleModal {function}: 모달 창 표시 상태 설정 함수
+ * contents {object}: 모달 창 내용 객체 (없을 경우 모달 창의 자식 컴포넌트가 렌더링됨)
+ * contents.title {string}: 모달 창 제목
+ * contents.content {string}: 모달 창 내용
+ * contents.actions {array}: 하단 버튼의 정보를 담는 객체 리스트
+ * contents.actions[].name {string}: 버튼에 표시되는 텍스트
+ * contents.actions[].color {string}: 버튼 색상 (생략 시 기본 색상으로)
+ * contents.actions[].action {function}: 버튼 클릭 시 호출되는 함수
+ */
+
 import ModalPortal from './Portal'
 import Button from './Button'
 import classes from './Modal.module.css'
