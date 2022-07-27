a3b0d9ddcbf783784deb37e4f05223ce11cca2a4
김성찬 <kimsc0714@naver.com>
2022-07-27 14:23:33 +0900

    refactor(sheet): change inline style prop to className prop

--- frontend/src/components/common/Sheet.js
+++ frontend/src/components/common/Sheet.js
@@ -10,29 +10,15 @@ import classes from './Sheet.module.css'
 // 참고
 // https://velog.io/@beberiche/React-props-props.children
 
-// size: 시트 패딩의 크기 (small: 16px, medium: 32px, large: 64px)
+// size: 시트 패딩의 크기 (small: 16px, medium(default): 32px, large: 64px)
 // shallow: 시트 그림자 범위 축소
 
 const Sheet = ({ children, size, shallow }) => {
-  let padding
-  switch (size) {
-    case 'small':
-      padding = '1rem'
-      break
-    case 'medium':
-      padding = '2rem'
-      break
-    case 'large':
-      padding = '4rem'
-      break
-    default:
-      padding = '2rem'
-  }
-
   return (
     <div
-      className={`${classes.sheet} ${shallow && classes.shallow}`}
-      style={{ padding }}
+      className={`${classes.sheet} ${size ? classes[size] : ''} ${
+        shallow ? classes.shallow : ''
+      }`}
     >
       {children}
     </div>
