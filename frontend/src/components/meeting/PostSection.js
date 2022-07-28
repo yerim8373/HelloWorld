86d001f49be7fc706547c7fec408db9c958093f4
김성찬 <kimsc0714@naver.com>
2022-07-28 15:52:35 +0900

    feat(postsection): add modal and click event handler

--- frontend/src/components/meeting/PostSection.js
+++ frontend/src/components/meeting/PostSection.js
@@ -1,13 +1,43 @@
 import PropTypes from 'prop-types'
 import Sheet from '../common/Sheet'
+import Modal from '../common/Modal'
+import { useState } from 'react'
 import classes from './PostSection.module.css'
 
 function PostItem({ post }) {
+  const [modalState, setModalState] = useState(false)
+  const handleModal = () => setModalState(!modalState)
+
+  const contents = {
+    title: post.title,
+    content: post.content,
+    subInfo: [
+      {
+        key: 'author',
+        name: '작성자',
+        content: post.author,
+      },
+      {
+        key: 'createdAt',
+        name: '작성일',
+        content:
+          post.createdAt + (post.updatedAt ? ` (수정: ${post.updatedAt})` : ''),
+      },
+    ],
+  }
+
   return (
-    <article className={classes.postItem}>
-      <h2 className={classes.title}>{post.title}</h2>
-      <p className={classes.summary}>{post.content}</p>
-    </article>
+    <>
+      <article className={classes.postItem} onClick={handleModal}>
+        <h2 className={classes.title}>{post.title}</h2>
+        <p className={classes.summary}>{post.content}</p>
+      </article>
+      <Modal
+        opened={modalState}
+        handleModal={handleModal}
+        contents={contents}
+      />
+    </>
   )
 }
 
@@ -40,16 +70,7 @@ PostItem.propTypes = {
 }
 
 PostSection.propTypes = {
-  posts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      content: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-      createdAt: PropTypes.string.isRequired,
-      updatedAt: PropTypes.string,
-    }).isRequired,
-  ).isRequired,
+  posts: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
 }
 
 export default PostSection
