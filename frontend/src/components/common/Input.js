40c253500e818aab9c87294adf9937c6ce106059
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-07-29 20:01:15 +0900

    feat(input.js): implement validation logic / create email, password valid function

--- frontend/src/components/common/Input.js
+++ frontend/src/components/common/Input.js
@@ -1,5 +1,6 @@
 import classes from './Input.module.css'
 import PropTypes from 'prop-types'
+import { useState, useRef } from 'react'
 // Input-Component /////////////////////////
 /// /////////////////////////////////////////
 /// /////////////////////////////////////////
@@ -12,20 +13,55 @@ import PropTypes from 'prop-types'
 // props.placeHolder (선택)
 // placeHolder 적용
 
-function Input({ id, type = 'text', placeholder = '' }) {
+function Input({ id, type = 'text', placeholder = '', onValid }) {
+  const inputRef = useRef()
+  const [errorComponent, setErrorComponent] = useState(null)
+
+  let colorInputClass = ''
+  let colorLabelClass = ''
+  switch (errorComponent) {
+    case null:
+      colorInputClass = ''
+      colorLabelClass = ''
+      break
+    case '':
+      colorInputClass = classes.valid_input_true
+      colorLabelClass = classes.valie_label_true
+      break
+    default:
+      colorInputClass = classes.valid_input_error
+      colorLabelClass = classes.valid_label_error
+  }
+
+  function inputValidHandler() {
+    let errorComponent = ''
+    for (let func in onValid) {
+      if (!onValid[func].func(inputRef.current.value)) {
+        errorComponent = <ErrorComponent text={onValid[func].message} />
+        break
+      }
+    }
+    setErrorComponent(errorComponent)
+  }
+
   return (
     <div className={classes.input_wrapper}>
-      <label className={classes.input_label} htmlFor={id}>
+      <label
+        className={`${classes.input_label} ${colorLabelClass}`}
+        htmlFor={id}
+      >
         {id}
       </label>
       <input
-        className={classes.input}
+        className={`${classes.input} ${colorInputClass}`}
         name={id}
         type={type}
         placeholder={placeholder}
+        onKeyUp={inputValidHandler}
+        ref={inputRef}
       />
       {/* 에레가 렌더링 되는 창 */}
-      <div></div>
+      {errorComponent && errorComponent}
     </div>
   )
 }
@@ -34,6 +70,15 @@ Input.propTypes = {
   id: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
+  onValid: PropTypes.object,
 }
 
 export default Input
+
+function ErrorComponent({ text }) {
+  return <div className={classes.error_component}>{text}</div>
+}
+
+ErrorComponent.propTypes = {
+  text: PropTypes.string.isRequired,
+}
