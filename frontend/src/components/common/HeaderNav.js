b9ce43c34f76ac62ce16f38cdb389ada7e5d9bd0
김성찬 <kimsc0714@naver.com>
2022-08-01 02:23:07 +0900

    feat(headernav): change header and item styles

--- frontend/src/components/common/HeaderNav.js
+++ frontend/src/components/common/HeaderNav.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import MenuBtn from './MenuBtn'
 import classes from './HeaderNav.module.css'
-import LogoImage from './LogoImage'
+import Logo from './Logo'
 import ProfileImage from './ProfileImage'
 import Button from './Button'
 import profile from '../../images/profile.jpg'
@@ -15,7 +15,7 @@ function HeaderNav() {
   // 후에 redux 공유객체로 바뀔 예정!
 
   // eslint-disable-next-line no-unused-vars
-  const [isLogined, setIsLogin] = useState(true)
+  const [isLogined, setIsLogin] = useState(false)
   const navigate = useNavigate()
 
   function routerPushHandler() {
@@ -24,44 +24,42 @@ function HeaderNav() {
   }
 
   return (
-    <nav className={classes.HeaderNav_nav}>
-      <div className={classes.HeaderNav_link_btns}>
-        <LogoImage color="logoWhite" width="50" height="50" />
+    <nav className={`${classes.headerNav} ${isLogined ? classes.auth : ''}`}>
+      <div className={classes.navWrapper}>
+        <div className={classes.HeaderNav_link_btns}>
+          <Logo withText={!isLogined} />
+          {isLogined && (
+            <div
+              className={`${classes.HeaderNav_link_btns} ${classes.hover_color}`}
+            >
+              <MenuBtn text="홈" link="/meeting" />
+
+              <MenuBtn
+                className={classes.HeaderNav_link_focus}
+                text="랜덤매칭"
+                link="/meeting/loading"
+              />
+            </div>
+          )}
+        </div>
+
         {isLogined ? (
+          <ProfileImage src={profile} size="small" />
+        ) : (
           <div
             className={`${classes.HeaderNav_link_btns} ${classes.hover_color}`}
           >
-            <MenuBtn text="홈" link="/meeting" />
-
-            <MenuBtn
-              className={classes.HeaderNav_link_focus}
-              text="랜덤매칭"
-              link="/meeting/loading"
+            <MenuBtn text="로그인" link="/auth" />
+            <Button
+              size="small"
+              text="회원가입"
+              color="success"
+              onEvent={routerPushHandler}
             />
+            {/* <MenuBtn text="회원가입" link="/auth/signup"></MenuBtn> */}
           </div>
-        ) : (
-          <NavLink to="/">
-            <h3 className={classes.HeaderNav_brand}>HelloWorld</h3>
-          </NavLink>
         )}
       </div>
-
-      {isLogined ? (
-        <ProfileImage src={profile} size="small" />
-      ) : (
-        <div
-          className={`${classes.HeaderNav_link_btns} ${classes.hover_color}`}
-        >
-          <MenuBtn text="로그인" link="/auth" />
-          <Button
-            size="small"
-            text="회원가입"
-            color="success"
-            onEvent={routerPushHandler}
-          />
-          {/* <MenuBtn text="회원가입" link="/auth/signup"></MenuBtn> */}
-        </div>
-      )}
     </nav>
   )
 }
