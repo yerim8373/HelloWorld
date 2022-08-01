fe57eacc6fb758117ddfaf885f7ff44b15a48f2c
김채윤 <only363me@gmail.com>
2022-08-02 08:30:03 +0900

    feat(signup page): signup page

--- frontend/src/components/auth/SignupForm3.js
+++ frontend/src/components/auth/SignupForm3.js
@@ -4,15 +4,21 @@ import Sheet from '../common/Sheet'
 import Input from '../common/Input'
 import Button from '../common/Button'
 import classes from './SignupForm.module.css'
-
+import SelectBox from '../common/SelectBox'
+import ProfileImage from '../common/ProfileImage'
+import ProfileImageSelector from '../common/ProfileImageSelector'
+import Checkbox from '../common/Checkbox'
 //////////////////////////////////////////////////////////////////
 function SignupForm3() {
   const navigate = useNavigate()
   function routerPushHandler() {
     navigate('/auth/signup4')
   }
+  function routerPushHandler2() {
+    navigate('/auth/signup2')
+  }
 
-  //////////////////////////////////////////////////////////////////
+  ///////////////////////
   return (
     <Sheet size="large">
       <form onSubmit={e => e.preventDefault()}>
@@ -27,24 +33,41 @@ function SignupForm3() {
               </font>
             </font>
           </div>
-          <p>이건 회원가입 333333</p>
+          <p>회원가입 3//selectbox css 수정 필요 </p>
           <div>
-            <Input id="이름" type="text" placeholder="example@example.com" />
+            <p className={classes.signup_input_label}>사용 언어</p>
+            <SelectBox></SelectBox>
           </div>
           <div>
-            <Input id="비밀번호" type="password" />
+            <p className={classes.signup_input_label}>프로필 이미지 </p>
+            <ProfileImageSelector></ProfileImageSelector>
           </div>
           <div>
-            <Input
-              id="비밀번호확인"
-              type="password"
-              placeholder="비밀번호 확인"
-            />
+            <Checkbox>
+              <p>
+                {' '}
+                <NavLink to="/auth/find-info">
+                  <span className={classes.signup_find_info}>
+                    {' '}
+                    개인정보 처리방침
+                  </span>
+                </NavLink>{' '}
+                에 동의합니다
+              </p>
+            </Checkbox>
           </div>
         </div>
         <div className={classes.signup_btns}>
           <div>
-            <Button onEvent={routerPushHandler} text="다음" />
+            <tr>
+              <td>
+                <Button onEvent={routerPushHandler2} text="이전" />
+              </td>
+              <td>
+                {' '}
+                <Button onEvent={routerPushHandler} text="다음" />
+              </td>
+            </tr>
           </div>
         </div>
       </form>
