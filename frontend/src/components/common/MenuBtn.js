4d0fd2c3392b2496cb60e0af302c36fc8c0ee360
김성찬 <kimsc0714@naver.com>
2022-08-01 15:06:19 +0900

    refactor(menu-btn): remove useless h4 tag

--- frontend/src/components/common/MenuBtn.js
+++ frontend/src/components/common/MenuBtn.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import Link from "next/link";
-// import { useRouter } from "next/router";
-
 import { NavLink, useMatch } from 'react-router-dom'
-
 import classes from './MenuBtn.module.css'
+
 const MenuBtn = ({ link, text }) => {
   const match = useMatch(link)
 
   return (
-    <NavLink to={link}>
-      <h4 className={`${classes.menuItem} ${match ? classes.active : ''}`}>
-        {text}
-      </h4>
+    <NavLink
+      to={link}
+      className={`${classes.menuItem} ${match ? classes.active : ''}`}
+    >
+      {text}
     </NavLink>
   )
 }
