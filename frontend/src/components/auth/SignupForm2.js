009538d01fb229446b8b82b428f1efcebe2dd454
김성찬 <kimsc0714@naver.com>
2022-08-03 17:53:12 +0900

    feat(route): combine prev & curr signup route

--- frontend/src/components/auth/SignupForm2.js
+++ frontend/src/components/auth/SignupForm2.js
@@ -1,103 +1,38 @@
-import React, { useState } from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
-import Sheet from '../common/Sheet'
+import PropTypes from 'prop-types'
 import Input from '../common/Input'
-import Button from '../common/Button'
+import RadioBtnGroup from '../common/RadioBtnGroup'
 import classes from './SignupForm.module.css'
 
-import Dropdown from '../common/Dropdown'
-
-import RadioBtnGroup from '../common/RadioBtnGroup'
-//단순히 넣기만 해도 오류뜹니다 => 다시확인하기
-
-//////////////////////////////////////////////////////////////////
-function SignupForm2() {
-  const navigate = useNavigate()
-  function routerPushHandler() {
-    navigate('/auth/signup3')
-  }
-  function routerPushHandler2() {
-    navigate('/auth/signup')
-  }
-
-  //////////////////////////////////////////////////////////////////
+function SignupStep2({ step }) {
   return (
-    <Sheet size="large">
-      <form onSubmit={e => e.preventDefault()}>
-        <div className={classes.signup_main}>
-          <h2 className={classes.signup_title}>회원가입</h2>
-          <div>
-            <font size="3" color="#f0b622">
-              {' '}
-              *
-              <font size="2" color="#7a8982">
-                은 필수 입력
-              </font>
-            </font>
-          </div>
-          <div>
-            <Input id="이름" type="text" placeholder="본명을 입력해주세요" />
-          </div>
-          <div>
-            <Input
-              id="닉네임"
-              type="text"
-              placeholder="2자 이상을 입력해주세요. 특수문자를 입력할 수 없어요"
-            />
-          </div>
-          <div>
-            <Input
-              id="휴대폰 번호"
-              type="text"
-              placeholder="전화번호 (하이픈 제외)"
-            />
-          </div>
-          <div></div>
-          <p className={classes.signup_input_label}>성별</p>
-          <div>
-            <tr>
-              <td>
-                <RadioBtnGroup
-                  items={[
-                    { name: '남자', value: '남자' },
-                    { name: '여자', value: '여자' },
-                    { name: '그 외', value: '그 외' },
-                  ]}
-                ></RadioBtnGroup>
-              </td>
-            </tr>
-          </div>
-          <div>
-            <tr>
-              <td>
-                <Input id="국적" type="text" placeholder="dropdown" />
-              </td>
-              <td>
-                {/* <Input id="나이" type="text" placeholder="나이" /> */}
-              </td>
-            </tr>
-          </div>
-        </div>
-        <div>
-          <div>
-            <tr>
-              <td className={classes.signup_table_width}>
-                <Button
-                  size="small"
-                  color="neutral"
-                  className={classes.button_error}
-                  onEvent={routerPushHandler2}
-                  text="이전"
-                />
-              </td>
-              <td className={classes.signup_table_width2}>
-                <Button size="small" onEvent={routerPushHandler} text="다음" />
-              </td>
-            </tr>
-          </div>
-        </div>
-      </form>
-    </Sheet>
+    <div className={`${classes.signupStepContainer} ${classes['step' + step]}`}>
+      <Input id="이름" type="text" placeholder="본명을 입력해주세요" />
+      <Input
+        id="닉네임"
+        type="text"
+        placeholder="2자 이상을 입력해주세요. 특수문자를 입력할 수 없어요"
+      />
+      <Input
+        id="휴대폰 번호"
+        type="text"
+        placeholder="전화번호 (하이픈 제외)"
+      />
+      <p className={classes.signup_input_label}>성별</p>
+      <RadioBtnGroup
+        name="gender"
+        items={[
+          { name: '남자', value: '남자' },
+          { name: '여자', value: '여자' },
+          { name: '그 외', value: '그 외' },
+        ]}
+      />
+      <Input id="나이" type="number" placeholder="나이를 입력해주세요" />
+    </div>
   )
 }
-export default SignupForm2
+
+SignupStep2.propTypes = {
+  step: PropTypes.number,
+}
+
+export default SignupStep2
--- frontend/src/pages/auth/signup2.js
+++
@@ -1,5 +0,0 @@
-import Signup2 from '../../components/auth/Signup2'
-
-export default function SignupPage2() {
-  return <Signup2></Signup2>
-}
