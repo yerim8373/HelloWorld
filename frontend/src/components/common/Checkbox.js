6e2cc967dfc0ca73083f3f7b8a397920aa58f394
김성찬 <kimsc0714@naver.com>
2022-08-04 13:42:57 +0900

    feat(checkbox): add locked prop

--- frontend/src/components/common/Checkbox.js
+++ frontend/src/components/common/Checkbox.js
@@ -1,29 +1,40 @@
-import { useState } from 'react'
 import PropTypes from 'prop-types'
-
 import classes from './Checkbox.module.css'
 
-export default function Checkbox({ id, children, checked }) {
-  const [checkState, setCheckState] = useState(checked ?? false)
-  const handleChange = e => setCheckState(e.target.checked)
+export default function Checkbox({
+  id,
+  children,
+  checked,
+  handleChange,
+  locked,
+}) {
+  const handleClick = e => {
+    if (locked) e.preventDefault()
+  }
 
   return (
-    <label htmlFor={id} className={classes.checkbox}>
+    <label
+      htmlFor={id}
+      className={`${classes.checkbox} ${locked ? classes.locked : ''}`}
+      onClick={handleClick}
+    >
       <input
         type="checkbox"
         id={id}
         className={classes.invisibleBox}
-        checked={checkState}
+        checked={checked}
         onChange={handleChange}
       />
       <div className={classes.visibleBox}>
-        <div className={classes.backgroundBox} />
+        <div
+          className={`${classes.backgroundBox} ${locked ? classes.locked : ''}`}
+        />
         <svg
           width="21"
           height="16"
           viewBox="0 0 21 16"
           xmlns="http://www.w3.org/2000/svg"
-          className={`${classes.checkIcon} ${checkState && classes.checked}`}
+          className={`${classes.checkIcon} ${checked ? classes.checked : ''}`}
         >
           <path
             fillRule="evenodd"
@@ -43,5 +54,7 @@ Checkbox.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]),
-  checked: PropTypes.bool,
+  checked: PropTypes.bool.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  locked: PropTypes.bool,
 }
