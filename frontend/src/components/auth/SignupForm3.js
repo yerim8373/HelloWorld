80d66491cc767540169fee9d112bd197912493fd
김성찬 <kimsc0714@naver.com>
2022-08-05 03:52:03 +0900

    feat(signup): render country flag when country is selected

--- frontend/src/components/auth/SignupForm3.js
+++ frontend/src/components/auth/SignupForm3.js
@@ -1,26 +1,47 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
+import codes from 'country-calling-code'
+import CountryFlag from 'react-country-flag'
 import Dropdown from '../common/Dropdown'
 import SelectBox from '../common/SelectBox'
 import classes from './SignupForm.module.css'
 
-const options = [
-  { value: 'value 1', label: 'value 1' },
-  { value: 'value 2', label: 'value 2' },
-  { value: 'value 3', label: 'value 3' },
-  { value: 'value 4', label: 'value 4' },
-  { value: 'value 5', label: 'value 5' },
-]
+const countries = codes.map(c => ({
+  label: c.country,
+  value: c.isoCode2,
+}))
 
 function SignupStep3({ step }) {
-  const handleChange = e => {
-    console.log(e)
-  }
+  const [country, setCountry] = useState('')
+  const handleChange = e => setCountry(e.target.value)
 
   return (
     <div className={`${classes.signupStepContainer} ${classes['step' + step]}`}>
-      <Dropdown id="국적" value="" data={options} onChange={handleChange} />
-      <p className={classes.signup_input_label}>사용 언어</p>
+      <div className={classes.countryContainer}>
+        <Dropdown
+          id="국적"
+          value={country}
+          items={countries}
+          handleChange={handleChange}
+          placeholder="국적을 선택해주세요"
+        />
+        {country ? (
+          <CountryFlag
+            countryCode={country}
+            svg
+            style={{
+              width: '3rem',
+              height: '2rem',
+            }}
+            title={country}
+            className={classes.countryFlag}
+          />
+        ) : (
+          <div className={classes.countryFlag}></div>
+        )}
+      </div>
 
+      <p className={classes.signup_input_label}>사용 언어</p>
       <SelectBox required />
     </div>
   )
