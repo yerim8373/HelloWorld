d48646be10bef1756f89ef218d61f3d699e2e470
김성찬 <kimsc0714@naver.com>
2022-08-11 21:09:02 +0900

    feat(axios): replace base url based on env file

--- frontend/src/store/auth-thunkActions.js
+++ frontend/src/store/auth-thunkActions.js
@@ -34,7 +34,7 @@ export const validToken = createAsyncThunk('auth/validToken', async token => {
 export const signup = createAsyncThunk('auth/signup', async userData => {
   try {
     const response = await axios.post(
-      'https://i7b106.p.ssafy.io/api/v1/user',
+      `${process.env.REACT_APP_API_URL}/api/v1/user`,
       userData,
     )
     return response.data
@@ -46,7 +46,7 @@ export const signup = createAsyncThunk('auth/signup', async userData => {
 export const logout = createAsyncThunk('auth/signout', async userData => {
   try {
     const response = await axios.post(
-      'https://i7b106.p.ssafy.io/api/v1/auth/signout',
+      `${process.env.REACT_APP_API_URL}/api/v1/auth/signout`,
       userData,
     )
     return response.data
