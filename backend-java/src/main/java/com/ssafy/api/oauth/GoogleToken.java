f24b140b5f159934e3649622139c74a22e0ffdde
홍주성 <hjs8482@naver.com>
2022-08-12 14:16:47 +0900

    refactor(oauth): kakao login complete

---
+++ backend-java/src/main/java/com/ssafy/api/oauth/GoogleToken.java
@@ -0,0 +1,29 @@
+package com.ssafy.api.oauth;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.minidev.json.JSONObject;
+
+@Getter
+@Setter
+public class GoogleToken implements SocialToken{
+    private String accessToken;
+    private String tokenType;
+    private String idToken;
+    private String scope;
+    private int expiresIn;
+    private String email;
+
+    public static GoogleToken of(JSONObject object){
+        GoogleToken token = new GoogleToken();
+
+        token.setAccessToken(object.get("access_token").toString());
+        token.setTokenType(object.get("token_type").toString());
+        token.setScope(object.get("scope").toString());
+        token.setIdToken(object.get("refresh_token").toString());
+        token.setExpiresIn(Integer.parseInt(object.get("expires_in").toString()));
+
+        return token;
+    }
+
+}
---
+++ backend-java/src/main/java/com/ssafy/api/oauth/NaverToken.java
@@ -0,0 +1,27 @@
+package com.ssafy.api.oauth;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.minidev.json.JSONObject;
+
+@Getter
+@Setter
+public class NaverToken implements SocialToken{
+    private String accessToken;
+    private String tokenType;
+    private String refreshToken;
+    private int expiresIn;
+    private String email;
+
+    public static NaverToken of(JSONObject object){
+        NaverToken token = new NaverToken();
+        token.setAccessToken(object.get("access_token").toString());
+        token.setTokenType(object.get("token_type").toString());
+        token.setRefreshToken(object.get("refresh_token").toString());
+        token.setExpiresIn(Integer.parseInt(object.get("expires_in").toString()));
+
+        return token;
+    }
+
+
+}
