74a09fb5cb6a702b50172d7822c427eb12861f36
김성찬 <kimsc0714@naver.com>
2022-08-15 00:35:21 +0900

    feat(settings): create password update request to server

--- frontend/src/components/settings/PasswordResetForm.jsx
+++ frontend/src/components/settings/PasswordResetForm.jsx
@@ -3,11 +3,21 @@ import Input from '../common/Input'
 import Button from '../common/Button'
 import classes from './PasswordResetForm.module.css'
 import {
+  passwordValidConfirmHandler,
   passwordValidIncludeLetterHandler,
   passwordValidLengthHandler,
 } from '../utils/validation/passwordValid'
-import { inputObj } from '../utils/helper/inputObj'
+import { passwordConfirmValidLengthHandler } from '../utils/validation/passwordConfirmValid'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  getLanguageData,
+  getUserData,
+  updateUser,
+} from '../../store/user-thunkActions'
 
+import countryData from '../utils/countries.json'
+
+// 비밀번호 유효성 검사 객체
 const passwordValidObj = {
   func0: {
     func: inputValue => passwordValidLengthHandler(inputValue),
@@ -15,56 +25,112 @@ const passwordValidObj = {
   },
   func1: {
     func: inputValue => passwordValidIncludeLetterHandler(inputValue),
-    message: '비밀번호는 영문자,숫자,특수문자를 포함해야 합니다',
+    message: '비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.',
+  },
+}
+
+// 비밀번호 확인 유효성 검사 객체
+const passwordConfirmValidObj = {
+  func0: {
+    func: inputValue => passwordConfirmValidLengthHandler(inputValue),
+    message: '비밀번호 확인은 8자 이상이어야 합니다.',
+  },
+  func1: {
+    func: inputValue => passwordValidIncludeLetterHandler(inputValue),
+    message: '비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.',
+  },
+  func2: {
+    func: (inputValue, target) =>
+      passwordValidConfirmHandler(inputValue, target),
+    message: '비밀번호가 일치하지 않습니다.',
   },
 }
 
 export default function PasswordResetForm() {
-  const [currPassword, setCurrPassword] = useState(inputObj)
-  const [newPassword, setNewPassword] = useState(inputObj)
-  const [newPasswordConfirm, setNewPasswordConfirm] = useState(inputObj)
+  const user = useSelector(state => state.user)
+  const { token } = useSelector(state => state.auth)
 
-  const isValid = obj => obj.value && obj.valid
+  const [currPassword, setCurrPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [newPasswordConfirm, setNewPasswordConfirm] = useState('')
 
-  // TODO: 비밀번호 일치 여부 유효성 검사 추가
-  const handleSubmit = e => {
-    e.preventDefault()
-    if (
-      isValid(currPassword) &&
-      isValid(newPassword) &&
-      isValid(newPasswordConfirm)
-    ) {
-      console.log('비밀번호 변경 가능')
-    } else {
-      console.log('비밀번호 변경 불가능')
+  const dispatch = useDispatch()
+
+  const handleSubmit = async e => {
+    try {
+      e.preventDefault()
+
+      if (!currPassword || !newPassword || !newPasswordConfirm) {
+        alert('모든 입력란을 채워주세요.')
+        return
+      }
+
+      const userData = {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        avatarSrc: user.avatar,
+        description: user.description,
+        nickname: user.nickname,
+        gender: user.gender,
+        age: user.age,
+        mobileNumber: user.mobileNumber,
+        country: countryData.find(c => c.code === user.country),
+        userLanList: user.languages,
+        pw: newPassword,
+      }
+      console.log(userData)
+
+      // 서버로 전송
+      const { payload } = await dispatch(updateUser({ token, userData }))
+      if (!payload || payload.result !== 'success') {
+        alert('서버에 문제가 발생했습니다. 잠시 후에 다시 시도해주세요.')
+        return
+      }
+
+      alert('비밀번호 변경이 완료되었습니다.')
+
+      setCurrPassword('')
+      setNewPassword('')
+      setNewPasswordConfirm('')
+    } catch (e) {
+      console.error(e)
+      alert('서버에 문제가 발생했습니다. 잠시 후에 다시 시도해주세요.')
     }
   }
 
   return (
-    <form className={classes.passwordResetForm} onSubmit={handleSubmit}>
+    <form
+      className={classes.passwordResetForm}
+      onSubmit={e => e.preventDefault()}
+    >
       <h1>비밀번호 변경</h1>
       <Input
         id="현재 비밀번호"
         type="password"
         placeholder="현재 비밀번호를 입력해주세요."
         onValid={passwordValidObj}
-        onData={pwd => setCurrPassword(pwd)}
+        onData={({ value, valid }) => valid && setCurrPassword(value)}
       />
       <Input
         id="새 비밀번호"
         type="password"
         placeholder="새 비밀번호를 입력해주세요."
         onValid={passwordValidObj}
-        onData={pwd => setNewPassword(pwd)}
+        onData={({ value, valid }) => valid && setNewPassword(value)}
+        required
       />
       <Input
         id="새 비밀번호 확인"
         type="password"
         placeholder="새 비밀번호를 한 번 더 입력해주세요."
-        onValid={passwordValidObj}
-        onData={pwd => setNewPasswordConfirm(pwd)}
+        onValid={passwordConfirmValidObj}
+        onData={({ value, valid }) => valid && setNewPasswordConfirm(value)}
+        required
+        meta={newPassword}
       />
-      <Button size="small" text="변경하기" />
+
+      <Button size="small" text="변경하기" onEvent={handleSubmit} />
     </form>
   )
 }
