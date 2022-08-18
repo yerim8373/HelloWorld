70e948b390097ae4ca08d87051b23e3129c92fbe
홍주성 <hjs8482@naver.com>
2022-08-19 00:47:52 +0900

    fix(heart): fix time, order and extention heart histories

--- backend-java/src/main/java/com/ssafy/db/repository/HeartHistoryRepository.java
+++ backend-java/src/main/java/com/ssafy/db/repository/HeartHistoryRepository.java
@@ -2,10 +2,6 @@ package com.ssafy.db.repository;
 
 import com.ssafy.db.entity.HeartHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface HeartHistoryRepository extends JpaRepository<HeartHistory, Long> {
-
 }
